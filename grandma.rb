e00dcbfd515bb0e5b1694f45b1398cfd08e753bb
Danny Wakeling <wakeling101@hotmail.com>
2019-02-18 15:44:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
-  elsif !string.upcase == string
+  elsif string.upcase == string
     return  "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
