b30458300df118a09b19ab7d8fa34cedaa0ebb35
Danny Wakeling <wakeling101@hotmail.com>
2019-02-18 15:40:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(string)
-  if string == I LOVE YOU GRANDMA!
-  puts "I LOVE YOU TOO PUMPKIN"
-elsif !string.upcase == string
-     puts "HUH?! SPEAK UP, SONNY!"
+  if string == "I LOVE YOU GRANDMA!"
+  return puts "I LOVE YOU TOO PUMPKIN"
+  elsif !string.upcase == string
+  return puts "HUH?! SPEAK UP, SONNY!"
   else puts "NO, NOT SINCE 1938!"
   end
 end
